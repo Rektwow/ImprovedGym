21f1f333e5329b7ecdb4f49b2c12ee70417a4736
Yassine Ben Mustapha <sae.yessin@outlook.com>
2023-08-31 22:28:24 +0200

    Updated gallery.js

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,18 +1,21 @@
 //selecting all required elements
-//const filterItem = document.querySelector(".co-items");
+const filterItem = document.querySelector(".co-items");
 const filterImg = document.querySelectorAll(".co-gallery .co-image");
 const allImages = document.querySelectorAll(".co-image img");
 const fullScreenImg = document.querySelector(".co-img img");
 // enlarge the image clicked
 allImages.forEach((elem) => {
-   elem.addEventListener("click", (e) => {  
-      fullScreenImg.src = e.target.src;
-   });
+  elem.addEventListener("click", (e) => {
+    fullScreenImg.src = e.target.src;
+  });
 });
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-window.onload = ()=>{ //after window loaded
-  filterItem.onclick = (selectedItem)=>{ //when clicked on co-items div
-    if(selectedItem.target.classList.contains("co-item")){ //wehn selected item has .co-item class
+window.onload = () => {
+  //after window loaded
+  filterItem.onclick = (selectedItem) => {
+    //when clicked on co-items div
+    if (selectedItem.target.classList.contains("co-item")) {
+      //wehn selected item has .co-item class
       filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
       selectedItem.target.classList.add("active"); //add that active class on selected item
       let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of selected item and store in filtername variable
@@ -20,14 +23,14 @@ window.onload = ()=>{ //after window loaded
         let filterImges = image.getAttribute("data-name"); //getting image data-name value
         //if selected item data-name value is equal to images data-name value
         //or selected item data-name value is equal to "all"
-        if((filterImges == filterName) || (filterName == "all")){
+        if (filterImges == filterName || filterName == "all") {
           image.classList.remove("hide"); //first remove the hide class from the image
           image.classList.add("show"); //add show class in image
-        }else{
+        } else {
           image.classList.add("hide"); //add hide class in image
           image.classList.remove("show"); //remove show class from the image
         }
       });
     }
-  }
-}
+  };
+};
